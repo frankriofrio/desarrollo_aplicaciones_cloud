6d846367abcad365c8405b127badc57f373b1e1e
frankriofrio <44818518+frankriofrio@users.noreply.github.com>
2022-10-14 23:39:30 -0500

    Add files via upload

---
+++ MiEjemplo.php
@@ -0,0 +1,19 @@
+<?php
+print("Esto es lenguaje PHP");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mi pagina en PHP</title>
+</head>
+<body>
+<form action="procesar.php" method="get">
+<input type="text" name="Usuario" placeholder="Ingresa User">
+<input type="text" name="Clave" placeholder="Ingresar Clave">
+<input type="text" name="Edad" placeholder="Ingresar Edad">
+<input type="submit" name="Enviar" value="Guardar">
+</form>
+Mi primer ejemplo PHP de Hoy.
+</body>
+</html>
