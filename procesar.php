9fafcd29c1bc0d74ef22af12f2ad5628cea3ca70
Francisco Riofrio <geovanny07121985@gmail.com>
2022-10-16 06:10:15 -0500

    Actualizar

--- procesar.php
+++
@@ -1,7 +0,0 @@
-<?php
-print_r($_REQUEST);
-print "<p>Su nombre es $_REQUEST[Usuario]</p>\n";
-print("<br>La clave es". $_REQUEST['Clave']);
-print("<h1>La clave es". $_REQUEST['Edad']);
-print("<h1>");
-?>
